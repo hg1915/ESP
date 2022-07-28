8e877c78147caa3b3aff3ea11657742da559fab7
HG HG <h.garabedian@outlook.com>
2022-07-27 19:57:49 -0700

    bug

--- ESP/URLBuilder.swift
+++ ESP/URLBuilder.swift
@@ -14,7 +14,7 @@ protocol URLBuilding {
 struct URLBuilder: URLBuilding {
     //"https://joinesp.quickbase.com/db/br8quzhyc?a=API_DoQuery&clist=36&query=%7b%2711%27.EX.%27today%27%7d&usertoken=b6xwrf_gbx2_0_ddbqj6gcfaqkuddg2ccstqtr246"
 
-    
+    //new url 
     //https://joinesp.quickbase.com/db/bqd4qrcpi?a=API_DoQuery&query=%7B%278%27.EX.%27today%27%7D&clist=167&usertoken=b6nziq_gbx2_0_bmdewcdcckv8tkhkhwhhtt4db2
     
     func recordsURL() -> String? {
