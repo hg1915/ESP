13919e0cc95128d3f0286ffa35eda7a60a5f4765
HG HG <h.garabedian@outlook.com>
2022-07-27 18:23:36 -0700

    Initial Commit

---
+++ ESP/ESPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESPApp.swift
+//  ESP
+//
+//  Created by GGTECH LLC on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ESPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
